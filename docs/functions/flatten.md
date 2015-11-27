895c2fff05c11a4ea9e8953f46547fc9cddea842
Eloi Poch <eloi.poch@gmail.com>
2015-11-27 19:37:42 +0100

    [function] flatten

---
+++ docs/functions/flatten.md
@@ -0,0 +1,24 @@
+# flatten
+
+## Description
+Flat a multidimensional collection
+
+## Parameters
+
+<dl>
+  <dt>coll</dt>
+  <dd>Collection of multiples dimensions.</dd>
+</dl>
+
+## Examples
+
+Flat a nested array of numbers:
+```php
+<?php
+
+use function Akamon\Phunctional\flatten;
+
+return flatten([1, [2, 3], [4, [5, 6], [[[[[7, 8]]]]]]]);
+            
+// => [1, 2, 3, 4, 5, 6, 7, 8]
+```
---
+++ tests/FlattenTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Akamon\Phunctional\Tests;
+
+use PHPUnit_Framework_TestCase;
+use function Akamon\Phunctional\flatten;
+
+class FlattenTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_should_flat_a_two_multidimensional_collection()
+    {
+        $actual = [[1], [2], [3], [4], [5]];
+
+        $this->assertSame([1, 2, 3, 4, 5], flatten($actual));
+    }
+
+    /** @test */
+    public function it_should_allow_one_multidimensional_collection()
+    {
+        $actual = [1, 2, 3, 4, 5];
+
+        $this->assertSame($actual, flatten($actual));
+    }
+
+    /** @test */
+    public function it_should_flat_nested_multidimensional_collection()
+    {
+        $actual = [1, [2, 3], [4, [5, 6], [[[[[7, 8]]]]]]];
+
+        $this->assertSame([1, 2, 3, 4, 5, 6, 7, 8], flatten($actual));
+    }
+}
