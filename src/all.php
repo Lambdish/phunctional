24ef284f4cd441baeba31f1f8219f9273a6940b6
Rafa Gómez <dotzecker@gmail.com>
2015-12-25 11:24:41 +0100

    Add `all` function docblock

--- src/all.php
+++ src/all.php
@@ -2,6 +2,18 @@
 
 namespace Akamon\Phunctional;
 
+use Traversable;
+
+/**
+ * Check if all the values of the collection satisfies the function
+ *
+ * @since 0.1
+ *
+ * @param callable          $fn   function to check if the predicate is true
+ * @param array|Traversable $coll collection of values to check all are true by the `$fn`
+ *
+ * @return bool
+ */
 function all(callable $fn, $coll)
 {
     return !some(not($fn), $coll);
