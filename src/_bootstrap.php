6495a1fe2f32ec0c364b4d6892d38daa7b50f95e
Rafa Gómez <dotzecker@gmail.com>
2015-12-28 15:33:57 +0100

    [functions] Add assoc

--- src/_bootstrap.php
+++ src/_bootstrap.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/all.php';
 require __DIR__ . '/any.php';
 require __DIR__ . '/apply.php';
+require __DIR__ . '/assoc.php';
 require __DIR__ . '/call.php';
 require __DIR__ . '/compose.php';
 require __DIR__ . '/filter.php';
---
+++ src/assoc.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Akamon\Phunctional;
+
+use Traversable;
+
+/**
+ * Associate a value to an array
+ *
+ * Passing a Generator to this function will work but it does not provide any improvement against a simple Traversable
+ * because to reach the last one is necessary iterate among all the items
+ *
+ * @param array|Traversable $coll  collection to assoc the value
+ * @param string            $key   the key the value will have
+ * @param string            $value the value to assoc
+ *
+ * @return array
+ */
+function assoc($coll, $key, $value)
+{
+    $array = $coll instanceof Traversable ? iterator_to_array($coll) : $coll;
+
+    $array[$key] = $value;
+
+    return $array;
+}
