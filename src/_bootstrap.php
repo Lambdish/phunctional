6d7e41beba1b846bfec5d37d436afad8014965eb
jorgeavila <jorge.avila@akamon.com>
2015-12-31 17:20:10 +0100

    do_if function

--- src/_bootstrap.php
+++ src/_bootstrap.php
@@ -8,6 +8,7 @@ require __DIR__ . '/butlast.php';
 require __DIR__ . '/complement.php';
 require __DIR__ . '/compose.php';
 require __DIR__ . '/dissoc.php';
+require __DIR__ . '/do_if.php';
 require __DIR__ . '/each.php';
 require __DIR__ . '/filter.php';
 require __DIR__ . '/first.php';
---
+++ tests/DoIfTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Akamon\Phunctional\Tests;
+
+use function Akamon\Phunctional\apply;
+use function Akamon\Phunctional\do_if;
+use PHPUnit_Framework_TestCase;
+
+final class DoIfTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_should_call_the_fn_if_predicates_are_true()
+    {
+        $this->assertEquals(5, apply(do_if($this->sumOne(), [$this->isOdd()]), [4]));
+    }
+
+    /** @test */
+    public function it_should_return_null_if_all_predicates_are_not_true()
+    {
+        $this->assertNull(apply(do_if($this->sumOne(), [$this->isGreaterThanOne(), $this->isOdd()]), [3]));
+    }
+
+    private function sumOne()
+    {
+        return function ($num) {
+            return $num + 1;
+        };
+    }
+
+    private function isOdd()
+    {
+        return function ($num) {
+            return $num % 2 === 0;
+        };
+    }
+
+    private function isGreaterThanOne()
+    {
+        return function ($num) {
+            return $num > 1;
+        };
+    }
+}
