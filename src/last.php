5d7ca3cfdded08b36564098a90fc4553955dc917
Rafa Gómez <dotzecker@gmail.com>
2016-02-22 22:40:54 +0100

    Add last function

---
+++ src/last.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Akamon\Phunctional;
+
+use Traversable;
+
+/**
+ * Returns the last element of a collection
+ *
+ * If the collection is empty returns null. If a generator is passed this will be iterated.
+ *
+ * @since 0.1
+ *
+ * @param array|Traversable $coll collection of values
+ *
+ * @return mixed|null
+ */
+function last($coll)
+{
+    return first(reverse($coll));
+}
