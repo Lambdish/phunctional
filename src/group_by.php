43b66784d9c5b68c40f1d5506ee55c19295d9795
Patrick <patrick91@live.it>
2019-06-19 09:45:00 +0200

    [function] group_by (#76)

---
+++ src/group_by.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Lambdish\Phunctional;
+
+use Generator;
+use Traversable;
+
+/**
+ * Returns an array with the items grouped by the results of applying $fn to each item.
+ *
+ * Function $fn should accept the value of the item as the first argument.
+ *
+ * @param callable                    $fn   function to apply to every item in the collection
+ * @param array|Traversable|Generator $coll collection of values to apply the function
+ *
+ * @return array
+ */
+function group_by(callable $fn, $coll)
+{
+    $result = [];
+
+    foreach ($coll as $item) {
+        $result[$fn($item)][] = $item;
+    }
+
+    return $result;
+}
