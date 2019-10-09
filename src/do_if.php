839de535545133329dccfbe9b757d97c938fd22a
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-09 15:00:23 +0200

    [do_if] Accepts predicates as variadic argument

--- src/do_if.php
+++ src/do_if.php
@@ -9,9 +9,9 @@ namespace Lambdish\Phunctional;
  * the callable arguments to the predicates is true for all of them.
  *
  * @param callable   $fn         Function to call if all predicates are valid.
- * @param callable[] $predicates Predicates to validate.
+ * @param callable   ...$predicates Predicates to validate.
  */
-function do_if(callable $fn, array $predicates): ?callable
+function do_if(callable $fn, callable ...$predicates): ?callable
 {
     return static function (...$args) use ($fn, $predicates) {
         $isValid = static function ($predicate) use ($args) {
